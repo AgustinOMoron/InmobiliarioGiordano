93f415126647e5e0bfad8407c4ee926c0e8cfcd0
Jana Viciana <95034415+JanaV8@users.noreply.github.com>
2026-06-24 20:56:44 -0300

    Delete find_cer.py

--- find_cer.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('monetarias.json', 'r', encoding='utf-16') as f:
-    data = json.load(f)
-
-for item in data.get('results', []):
-    desc = item.get('descripcion', '').upper()
-    if 'CER' in desc or 'COEFICIENTE' in desc or 'ESTABILIZA' in desc:
-        print(f"ID: {item['idVariable']} - {item['descripcion']} (Desde: {item['primerFechaInformada']}, Hasta: {item['ultFechaInformada']})")
